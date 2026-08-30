812e2c8724c34631c9f99d7715dbc9a7429fc63c
Abhinav Chaudhary <gamingdickens@gmail.com>
2026-08-30 21:49:25 +0530

    Link for backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,10 @@
-import axios from 'axios';
-const api=axios.create({baseURL:import.meta.env.VITE_API_URL||'http://localhost:5000/api'});
-api.interceptors.request.use(c=>{const t=localStorage.getItem('na_token');if(t)c.headers.Authorization=`Bearer ${t}`;return c});
+import axios from "axios";
+const api = axios.create({
+  baseURL: "https://neon-aura-8nuc.onrender.com",
+});
+api.interceptors.request.use((c) => {
+  const t = localStorage.getItem("na_token");
+  if (t) c.headers.Authorization = `Bearer ${t}`;
+  return c;
+});
 export default api;
